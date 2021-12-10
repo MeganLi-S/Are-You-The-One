87344a7b8d4df0e51837c0e933dfe20e51f51ee6
MeganLi-S <83846607+MeganLi-S@users.noreply.github.com>
2021-12-10 09:01:50 -0500

    Add files via upload

---
+++ Names.py
@@ -0,0 +1,24 @@
+list_of_names = ['Amber', 'Austin', 'Allen', 'August', 'Benny', 'Brandon', 'Brennan', 'Bailey', 'Beatrice', 'Bonnie', 'Clyde',
+                  'Connor', 'Claire', 'Cynthia', 'Danny', 'Delia', 'Delilah', 'Devin', 'Ethan', 'Evan', 'Emily', 'Emma', 'Frank'
+                  , 'Frannie', 'Felipe', 'Felicity', 'George', 'Gail', 'Gavin', 'Gabriella', 'Henry', 'Holly', 'Hank', 'Hailey',
+                  'Hadley', 'Ian', 'Isabelle', 'John', 'Julia', 'Jacob', 'Jessica', 'Joe', 'Jennifer', 'Joseph', 'Jackie', 'Kelly',
+                  'Kevin', 'Kyle', 'Kim', 'Lawrence', 'Lucy', 'Luke', 'Lily', 'Matthew', 'May', 'Marcy', 'Marty', 'Nick', 'Natalie',
+                  'Nora', 'Noah', 'Nelly', 'Owen', 'Ophelia', 'Oscar', 'Ola', 'Penelope', 'Piper', 'Peter', 'Patrick', 'Quincy',
+                  'Quinn', 'Robert', 'Rachel', 'Ryder', 'Reilly', 'Sally', 'Shawn', 'Susan', 'Samuel', 'Tyler', 'Tristan', 'Taylor',
+                  'Talia', 'Tiffany', 'Trinity', 'Theo', 'Thomas', 'Timothy', 'Victory', 'Vicky', 'Victoria', 'Vincent', 'William',
+                  'Wendy', 'Wallis', 'Willow', 'Wesley', 'Walter', 'Winter', 'Wyatt', 'Whitney', 'Willa', 'Xavier', 'Yolanda', 
+                  'Zeke', 'Zoey', 'Adam', "Harry"]
+
+
+sixteen_names = ['joe', 'jane', 
+                'toby', 'nate', 
+                'jess', 'jen', 
+                'abby', 'manny', 
+                'john', 'owen', 
+                'piper', 'willa', 
+                'vincent', 'marley',
+                'indigo', 'harry']
+
+
+print(len(list_of_names))
+
